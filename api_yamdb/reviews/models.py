5fb2953e619048a3a43e9e16a81ef2e2f4c313a0
Alexandr Bodnari <infidew666@gmai.com>
2023-06-27 21:17:11 +0100

    Исправлены null=True и проставлены правильные значения max_length

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(
-        max_length=200,
-        verbose_name='Название'
+        max_length=256,
+        verbose_name='Название категории'
     )
     slug = models.SlugField(
         max_length=50,
-        verbose_name='Идентификатор'
+        verbose_name='Идентификатор категории'
     )
 
     def __str__(self):
@@ -17,12 +17,12 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(
-        max_length=200,
-        verbose_name='Название'
+        max_length=256,
+        verbose_name='Название жанра'
     )
     slug = models.SlugField(
         max_length=50,
-        verbose_name='Идентификатор'
+        verbose_name='Идентификатор жанра'
     )
 
     def __str__(self):
@@ -31,18 +31,20 @@ class Genre(models.Model):
 
 class Title(models.Model):
     name = models.CharField(
-        max_length=200,
-        verbose_name='Название'
+        max_length=256,
+        verbose_name='Название произведения'
     )
     year = models.IntegerField(
-        verbose_name='Год выпуска'
+        verbose_name='Год выпуска',
+
     )
     description = models.TextField(
-        verbose_name='Описание'
+        verbose_name='Описание произведения',
+        null=True,
+        blank=True
     )
     genre = models.ManyToManyField(
         Genre,
-        max_length=50,
         verbose_name='Жанр',
         through='GenreTitle'
     )
@@ -51,7 +53,6 @@ class Title(models.Model):
         verbose_name='Категория',
         on_delete=models.SET_NULL,
         related_name='titles',
-        null=True
     )
 
     def __str__(self):
