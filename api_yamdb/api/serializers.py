8ca74a4b4f1b348598f566d363a7a55cc9009243
Alexandr Bodnari <infidew666@gmai.com>
2023-06-27 21:01:43 +0100

    Созданы сериализаторы (не до конца)

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from reviews.models import (Category,
+                            Genre,
+                            Title)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Title
