a3ad398c0f41c8fcf23187ade14edbe6654ca3cc
Alexandr Bodnari <infidew666@gmai.com>
2023-06-27 15:20:20 +0100

    Добавлены модели Category, Genre, Title, GenreTitle

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        verbose_name='Идентификатор'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        verbose_name='Идентификатор'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    year = models.IntegerField(
+        verbose_name='Год выпуска'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    genre = models.CharField(
+        max_length=50,
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        verbose_name='Жанр'
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+
+    def __str__(self):
+        return f'У произведения "{self.title}", жанр: {self.genre}'
