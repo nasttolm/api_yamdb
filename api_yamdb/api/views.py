ef0f24753dff5e40f50548d02e8d5ff65bce76be
Alexandr Bodnari <infidew666@gmai.com>
2023-06-28 16:04:28 +0100

    Созданы вьюсеты для Category, Genre, Title (В сыром виде)

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from reviews.models import (Category,
+                            Genre,
+                            Title)
+from .serializers import (CategorySerializer,
+                          GenreSerializer,
+                          TitleSerializer)
+from .permissions import AdminOrReadOnly
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (AdminOrReadOnly, )
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (AdminOrReadOnly, )
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (AdminOrReadOnly, )
