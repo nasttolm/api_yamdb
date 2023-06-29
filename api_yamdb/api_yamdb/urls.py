8e87fce5dbb43e662f66200161beed6447dddbbd
Alexandr Bodnari <infidew666@gmai.com>
2023-06-29 11:33:29 +0100

    Созданы эндпоинты для Category, Genre и Title

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -23,6 +23,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
