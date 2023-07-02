54ffd575e4382f39b36f6bddac7247c357af09ab
Alexandr Bodnari <infidew666@gmai.com>
2023-07-02 14:38:19 +0100

    Скрипт готов, нужно разобраться с полем title_id в модели Comment

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -21,13 +21,61 @@ def import_data():
                 author_id=row['author'],
                 pub_date=row['pub_date'],
             )
-            
+
     with open('static/data/category.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             Category.objects.create(
                 name=row['name'],
                 slug=row['slug']
+            )
+
+    with open('static/data/genre_title.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            GenreTitle.objects.create(
+                title_id=row['title_id'],
+                genre_id=row['genre_id']
+            )
+
+    with open('static/data/genre.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Genre.objects.create(
+                name=row['name'],
+                slug=row['slug']
+            )
+
+    with open('static/data/review.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Review.objects.create(
+                title_id_id=row['title_id'],
+                text=row['text'],
+                author_id=row['author'],
+                score=row['score'],
+                pub_date=row['pub_date']
+            )
+
+    with open('static/data/titles.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            Title.objects.create(
+                name=row['name'],
+                year=row['year'],
+                category_id=row['category']
+            )
+
+    with open('static/data/users.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            User.objects.create(
+                username=row['username'],
+                email=row['email'],
+                role=row['role'],
+                bio=row['bio'],
+                first_name=row['first_name'],
+                last_name=row['last_name']
             )
 
 
