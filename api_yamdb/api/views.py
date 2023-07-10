e6626dafd9760b62541e84ee23082aba5a3b484d
Saeed Amirov <saeed-starik@mail.ru>
2023-07-10 19:25:57 +0300

    исправления по отзывам и комментариям

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -202,12 +202,13 @@ class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (AuthorAdminModerOrReadOnly,
                           permissions.IsAuthenticatedOrReadOnly)
+    pagination_class = Pagination
 
     def get_title(self):
         return get_object_or_404(Title, pk=self.kwargs.get('title_id'))
 
     def get_queryset(self):
-        return self.get_title().reviews
+        return self.get_title().reviews.all()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, title=self.get_title())
